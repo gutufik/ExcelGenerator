76137c83d2d7c605cf8f8cc3be71ae741a01c796
Михаил Мясников <gutufik@mail.ru>
2022-04-06 22:02:16 +0300

    Display data

--- ExcelGenerator/Program.cs
+++ ExcelGenerator/Program.cs
@@ -11,12 +11,22 @@ namespace ExcelGenerator
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             var users = new ObservableCollection<user>(kepartners_data2Entities.GetContext().user);
             foreach (var user in users)
             {
-                foreach (var item in user.activity)
+                if (user.email.Contains('@'))
                 {
-                    Console.WriteLine($"{user.email} {item.title}");
+                    foreach (var activity in user.activity)
+                    {
+                        foreach (var category in activity.category)
+                        {
+                            foreach (var item in category.registry)
+                            {
+                                Console.WriteLine($"{user.email} {category.title} {item.date} {item.money} {item.comment}");
+                            }
+                        }
+                    }
                 }
             }
         }
