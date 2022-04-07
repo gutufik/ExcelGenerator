0fb8e7567ba86d7ac2913a138e2b37f279e2ae54
Михаил Мясников <gutufik@mail.ru>
2022-04-07 20:39:36 +0300

    try generate excels

--- ExcelGenerator/Program.cs
+++ ExcelGenerator/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ExcelGenerator
 {
@@ -11,22 +12,47 @@ namespace ExcelGenerator
     {
         static void Main(string[] args)
         {
+            var application = new Excel.Application();
+
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             var users = new ObservableCollection<user>(kepartners_data2Entities.GetContext().user);
             foreach (var user in users)
             {
                 if (user.email.Contains('@'))
                 {
-                    foreach (var activity in user.activity)
+                    if (user.activity.Count == 0)
+                        continue;
+                    
+                    for (int i = 0; i < user.activity.Count; i++)
                     {
-                        foreach (var category in activity.category)
+                        application.SheetsInNewWorkbook = user.activity.ToList()[i].category.Count;
+
+                        Excel.Workbook workbook = application.Workbooks.Add(Type.Missing);
+
+                        for (int c = 0; c < user.activity.ToList()[i].category.Count; c++)
                         {
-                            foreach (var item in category.registry)
+                            var category = user.activity.ToList()[i].category.ToList()[c];
+                            Excel.Worksheet worksheet = application.Worksheets.Item[c + 1];
+                            //Console.WriteLine($"{user.email} {category.title} {item.date} {item.money} {item.comment}");
+                            worksheet.Name = $"{category.title}{c}";
+
+                            var rowIndex = 1;
+
+                            for (int j = 0; j < category.registry.Count(); j++)
                             {
-                                Console.WriteLine($"{user.email} {category.title} {item.date} {item.money} {item.comment}");
+                                worksheet.Cells[1][rowIndex] = category.title;
+                                worksheet.Cells[2][rowIndex] = category.registry.ToList()[j].date;
+                                worksheet.Cells[3][rowIndex] = category.registry.ToList()[j].comment;
+                                rowIndex++;
                             }
+                            worksheet.Columns.AutoFit();
+                            worksheet.Rows.AutoFit();
                         }
                     }
+                    application.Visible = true;
+                    //application.ActiveWorkbook.SaveAs();
+                    application.ActiveWorkbook.SaveAs($"D:\\Users\\gutuf\\Desktop\\excels\\{user.email}.xlsx");
+                    application.ActiveWorkbook.Close();
                 }
             }
         }
